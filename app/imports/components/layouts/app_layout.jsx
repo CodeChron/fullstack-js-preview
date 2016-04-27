0c21b9b5e55bf0934a1fa3385750cdeeb69179e4
Anders Ramsay <andersr@gmail.com>
2016-04-27 14:07:38 -0400

    react setup

---
+++ app/imports/components/layouts/app_layout.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const AppLayout = () =>
+  <div id="app-container">
+    <div id="main-content">
+      React placeholder
+    </div>
+  </div>
--- app/imports/startup/client/index.js
+++ app/imports/startup/client/index.js
@@ -1,1 +1,2 @@
+import './main.html'
 import './main.js'
