47b22e06ed34c87b01ec6cf0a11e91eb8768993f
Anders Ramsay <andersr@gmail.com>
2016-04-29 10:56:04 -0400

    twbs styling

--- app/imports/components/layouts/app_header_layout.jsx
+++ app/imports/components/layouts/app_header_layout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 export const AppHeaderLayout = (props) =>
-  <div id="app-header">
-	<div className="header-center">
-      <h1 className="page-title">{props.pageTitle}</h1>
-	</div>
-  </div>
+  <nav id="app-header" className="navbar navbar-default">
+    <div className="container-fluid">
+      <div className="navbar-header">
+        <h1 className="navbar-brand">{props.pageTitle}</h1>
+      </div>
+    </div>
+  </nav>
 
 AppHeaderLayout.propTypes = {
   pageTitle: React.PropTypes.string
