1e8521b79781ddf9c7ea3b06629a855356fa4ef3
Anders Ramsay <andersr@gmail.com>
2016-05-09 11:44:30 -0400

    app header layout comments

--- app/imports/components/layouts/app_header_layout.jsx
+++ app/imports/components/layouts/app_header_layout.jsx
@@ -10,10 +10,12 @@ export const AppHeaderLayout = (props) =>
 </nav>
 
 
+//This list the props accepted by this component and their type, which can work as a mini API
 AppHeaderLayout.propTypes = {
   pageTitle: React.PropTypes.string
 }
 
+//Here, default values can be set for props that were not given a value when the component was invoked
 AppHeaderLayout.defaultProps = { 
   pageTitle: "My Notes App"
 }
