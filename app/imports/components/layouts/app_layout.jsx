e342bb72f88da026e215b65b4586135f19a36336
Anders Ramsay <andersr@gmail.com>
2016-05-09 11:28:01 -0400

    long form version of AppLayout

--- app/imports/components/layouts/app_layout.jsx
+++ app/imports/components/layouts/app_layout.jsx
@@ -6,3 +6,15 @@ export const AppLayout = () =>
       React placeholder
     </div>
   </div>
+
+
+//The above is the same as:
+// var AppLayout = function() {
+// 	return (
+//     React.createElement('div', {id: "app-container"},
+//       React.createElement('div', {id: "main-content", className:"container"}, "React Placeholder")
+//     )
+// 	)
+// }
+
+// export { AppLayout }
