1fc83e0cafa671e24b3840a3cab77d71baa244a3
Anders Ramsay <andersr@gmail.com>
2016-05-09 11:48:22 -0400

    AppHeaderLayout comments

--- app/imports/components/layouts/app_layout.jsx
+++ app/imports/components/layouts/app_layout.jsx
@@ -1,7 +1,9 @@
-//This is ES6/ES2015 syntax, which Meteor transpiles (using Babel) out of the box
 import React from 'react'
+
+//import components we'll be using
 import { AppHeaderLayout } from './app_header_layout'
 
+//insert a component just as if it were an html element
 export const AppLayout = () =>
   <div id="app-container">
     <AppHeaderLayout />
@@ -9,14 +11,3 @@ export const AppLayout = () =>
       React placeholder
     </div>
   </div>
-
-//The above would be the same as:
-// var AppLayout = function() {
-// 	return (
-//     React.createElement('div', {id: "app-container"},
-//       React.createElement('div', {id: "main-content", className:"container"}, "React Placeholder")
-//     )
-// 	)
-// }
-
-// export { AppLayout }
